88d0a6a5859bf757b9717da7f29b1a3bb26cf0eb
Mohsen Malekifard <mohsenmaleki1389@gmail.com>
2025-08-05 15:29:15 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const name = "Mohsen";
+const age = 15;
+
+let isAdult;
+
+age >= 18 ? isAdult = true : isAdult = false;
+
+console.log(`${name} is ${age} years old.`);
+console.log(`Adult: ${isAdult}`);
